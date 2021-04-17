c7975940b9b1e09ecc41d807c7bd40c10d3e9b9e
davigotxi <davigotxi@gmail.com>
2021-04-17 14:32:17 +0700

    rename repo name

--- cmd/blueprint/main_test.go
+++ cmd/blueprint/main_test.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/MartinHeinz/go-project-blueprint/cmd/blueprint/config"
+	"github.com/davigotxi/go-project-blueprint/cmd/blueprint/config"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
